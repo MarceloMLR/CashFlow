8dbf8fa4fa07bad66a67269a2b7e74ffa3c6c1f8
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-05-27 23:40:00 -0300

    feat: Criado endpoint de registro de despesas e communication

---
+++ main/CashFlow.API/Controllers/CashFlowBaseController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+[Route("cashflow/[controller]")]
+[ApiController]
+public class CashFlowBaseController : ControllerBase
+{
+}
---
+++ main/CashFlow.API/Controllers/ExpensesController.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+public class ExpensesController : CashFlowBaseController
+{
+    [HttpPost]
+
+    public IActionResult Register(RequestExpenseJson request)
+    {
+        return Ok();
+    }
+}
