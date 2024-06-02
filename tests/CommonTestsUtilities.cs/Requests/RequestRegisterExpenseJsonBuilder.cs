ea83cb3fac18208ae8845c818d72e69343207412
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-06-02 20:19:01 -0300

    feat: Criado project de testes e common utilities e adicionado a lib Bogus para geração de dados de teste

---
+++ tests/CommonTestsUtilities.cs/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestsUtilities.cs.Requests;
+
+
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestExpenseJson Build()
+    { 
+        return new Faker<RequestExpenseJson>()
+        .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+        .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+        .RuleFor(r => r.Date, faker => faker.Date.Past())
+        .RuleFor(r => r.Amount, faker => faker.Random.Decimal(min: 1, max: 1000))
+        .RuleFor(r => r.Type, faker => faker.PickRandom<PaymentType>());
+    }
+}
---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,22 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CommonTestsUtilities.cs.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.True(result.IsValid); 
+    }
+}
