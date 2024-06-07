94969ff6812e040413361cb6e785e1f160481c15
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-06-07 10:54:01 -0300

    Refactoring: Refatorado o metodo de exception para atender o principio Open-Closed do SOLID

--- main/CashFlow.API/Filters/ExceptionFilter.cs
+++ main/CashFlow.API/Filters/ExceptionFilter.cs
@@ -21,28 +21,14 @@ public class ExceptionFilter : IExceptionFilter
     }
 
     private void HandleProjectException(ExceptionContext context)
+
     {
-        if (context.Exception is ErrorOnValidationException errorOnValidationException)
-        {
-            var errorResponse = new ResponseErrorJson(errorOnValidationException.Errors);
+        var cashFlowException = context.Exception as CashFlowException;
+        context.HttpContext.Response.StatusCode = cashFlowException!.StatusCode;
 
-            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-            context.Result = new BadRequestObjectResult(errorResponse);
-        }
-        else if (context.Exception is NotFoundException notFoundException)
-        {
-            var errorResponse = new ResponseErrorJson(notFoundException.Message);
-
-            context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-            context.Result = new NotFoundObjectResult(errorResponse);
-        }
-        else
-        {
-            var errorResponse = new ResponseErrorJson(context.Exception.Message);
-
-            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-            context.Result = new BadRequestObjectResult(errorResponse);
-        }
+        var errorResponse = new ResponseErrorJson(cashFlowException.GetErrors());
+        context.Result = new ObjectResult(errorResponse);
+        
     }
 
     private void ThrowUnknowError(ExceptionContext context) 
