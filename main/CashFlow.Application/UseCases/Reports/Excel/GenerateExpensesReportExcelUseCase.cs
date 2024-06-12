1148be742fcf92868e3937dc98067da983c56652
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-06-12 14:37:36 -0300

    Feat: Adicionado simbolo de moeda no relatorio e adicionado using na instancia do XLWorkbook

--- main/CashFlow.Application/UseCases/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ main/CashFlow.Application/UseCases/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -3,11 +3,13 @@ using CashFlow.Domain.Enums;
 using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
 using ClosedXML.Excel;
+using DocumentFormat.OpenXml.VariantTypes;
 
 namespace CashFlow.Application.UseCases.Reports.Excel
 {
     public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
     {
+        private const string CURRENCY_SYMBOL = "R$";
         private readonly IExpensesReadOnlyRepository _repository;
         public GenerateExpensesReportExcelUseCase(IExpensesReadOnlyRepository repository)
         {
@@ -22,7 +24,7 @@ namespace CashFlow.Application.UseCases.Reports.Excel
                 return [];
             }
 
-            var workbook = new XLWorkbook();
+            using var workbook = new XLWorkbook();
 
             workbook.Author = "Marcelo Lima";
             workbook.Style.Font.FontSize = 12;
@@ -41,6 +43,7 @@ namespace CashFlow.Application.UseCases.Reports.Excel
                 worksheet.Cell($"B{raw}").Value = expense.Date;
                 worksheet.Cell($"C{raw}").Value = ConvertPaymentType(expense.PaymentType);
                 worksheet.Cell($"D{raw}").Value = expense.Amount;
+                worksheet.Cell($"D{raw}").Style.NumberFormat.Format = $"- {CURRENCY_SYMBOL} #";
                 worksheet.Cell($"E{raw}").Value = expense.Description;
 
                 raw++;
