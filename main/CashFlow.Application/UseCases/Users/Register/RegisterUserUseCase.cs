ccb7fa8484d3b5994e7223676c6530d7c6317b8b
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-06-20 12:49:36 -0300

    Feat: Adicionado validação se o email já está em uso

--- main/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
+++ main/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Domain.Repositories.Users;
 using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionBase;
+using FluentValidation.Results;
 
 namespace CashFlow.Application.UseCases.Users.Register;
 
@@ -11,14 +14,16 @@ public class RegisterUserUseCase : IRegisterUserUseCase
 {
     private readonly IMapper _mapper;
     private readonly IPasswordEncripter _passwordEncripter;
-    public RegisterUserUseCase(IMapper mapper, IPasswordEncripter encripter)
+    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+    public RegisterUserUseCase(IMapper mapper, IPasswordEncripter encripter, IUserReadOnlyRepository userReadOnlyRepository)
     {
         _mapper = mapper;
         _passwordEncripter = encripter;
+        _userReadOnlyRepository = userReadOnlyRepository;
     }
     public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
     {
-        Validate(request);
+        await Validate(request);
 
         var user =  _mapper.Map<Domain.Entities.User>(request);
         user.Password = _passwordEncripter.Encrypt(request.Password);
@@ -29,11 +34,18 @@ public class RegisterUserUseCase : IRegisterUserUseCase
         };
     }
 
-    private void Validate(RequestRegisterUserJson request)
+    private async Task Validate(RequestRegisterUserJson request)
     {
         var result = new RegisterUserValidator().Validate(request);
 
-        if (result is null) 
+        var emailExists = await _userReadOnlyRepository.ExistActiveUserWithEmail(request.Email);
+
+        if (emailExists)
+        {
+            result.Errors.Add(new ValidationFailure(string.Empty, ResourceErrorMessages.EMAIL_ALREADY_REGISTERED));
+        }
+
+        if (result.IsValid == false) 
         {
             var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
             
--- main/CashFlow.Exception/ResourceErrorMessages.Designer.cs
+++ main/CashFlow.Exception/ResourceErrorMessages.Designer.cs
@@ -70,6 +70,15 @@ namespace CashFlow.Exception {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to The e-mail is already be used.
+        /// </summary>
+        public static string EMAIL_ALREADY_REGISTERED {
+            get {
+                return ResourceManager.GetString("EMAIL_ALREADY_REGISTERED", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Email is empty.
         /// </summary>
         public static string EMAIL_EMPTY {
