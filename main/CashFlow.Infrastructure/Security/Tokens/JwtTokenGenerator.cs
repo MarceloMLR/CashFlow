8718ef0f95f1f84f22dab20ce861501f3ac1e46b
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-06-21 11:32:20 -0300

    Feat: Criado geração do JwtToken

---
+++ main/CashFlow.Infrastructure/Security/Tokens/JwtTokenGenerator.cs
@@ -0,0 +1,49 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Tokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CashFlow.Infrastructure.Security.Tokens
+{
+    public class JwtTokenGenerator : IAcessTokenGenerator
+    {
+        private readonly uint _expirationTimeMinutes;
+        private readonly string _signingKey;
+
+        public JwtTokenGenerator(uint expirationTimeMinutes, string signingKey)
+        {
+            _expirationTimeMinutes = expirationTimeMinutes;
+            _signingKey = signingKey;
+        }
+        public string Generate(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var claims = new List<Claim>();
+            {
+                new Claim(ClaimTypes.Name, user.Name);
+                new Claim(ClaimTypes.Sid, user.Guid.ToString());
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+               Expires = DateTime.UtcNow.AddMinutes(_expirationTimeMinutes),
+               SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature),
+               Subject = new ClaimsIdentity(claims)
+            };
+
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(securityToken);
+        }
+
+        private SymmetricSecurityKey SecurityKey()
+        {
+            var key = Encoding.UTF8.GetBytes(_signingKey);
+
+            return new SymmetricSecurityKey(key);
+        } 
+    }
+}
