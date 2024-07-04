e6179947f7fa5be76ac2b31ac740a97f5acf44db
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-07-04 10:30:28 -0300

    Test: Finalizado teste de sucesso para o endpoint de registro de usuario

--- tests/WebApi.Tests/User/Register/RegisterUserTest.cs
+++ tests/WebApi.Tests/User/Register/RegisterUserTest.cs
@@ -1,8 +1,8 @@
 ﻿using CommonTestsUtilities.cs.Requests;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 using Xunit;
 
 namespace WebApi.Tests.User.Register
@@ -13,18 +13,24 @@ namespace WebApi.Tests.User.Register
         private const string METHOD = "cashflow/user";
         public RegisterUserTest(CustomWebApplicationFactory webApplicationFactory)
         {
-            _httpClient = webApplicationFactory.CreateClient();
-           
+            _httpClient = webApplicationFactory.CreateClient();         
         }
 
-        [Fact]
+        [Fact]  
         public async Task Success()
         {
            var request = RequestRegisterUserJsonBuilder.Build();
 
            var result = await _httpClient.PostAsJsonAsync(METHOD, request);
 
-            result.StatusCode.Should().Be(HttpStatusCode.Created);
+           result.StatusCode.Should().Be(HttpStatusCode.Created);
+
+           var body = await result.Content.ReadAsStreamAsync();
+
+            var response = await JsonDocument.ParseAsync(body);
+
+            response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
+            response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
 
         }
     }
