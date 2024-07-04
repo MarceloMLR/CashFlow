8ed9f19be260b599e97b6fefdb5a208f210d0659
Marcelo Lima <marcelolimaribeiro2021@gmail.com>
2024-07-03 23:28:43 -0300

    Test: Criado teste de integração de sucesso para registro de usuario

--- tests/WebApi.Tests/User/Register/RegisterUserTest.cs
+++ tests/WebApi.Tests/User/Register/RegisterUserTest.cs
@@ -1,5 +1,8 @@
 ﻿using CommonTestsUtilities.cs.Requests;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
 using Xunit;
 
 namespace WebApi.Tests.User.Register
@@ -7,16 +10,21 @@ namespace WebApi.Tests.User.Register
     public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly HttpClient _httpClient;
+        private const string METHOD = "cashflow/user";
         public RegisterUserTest(WebApplicationFactory<Program> webApplicationFactory)
         {
             _httpClient = webApplicationFactory.CreateClient();
+           
         }
 
         [Fact]
         public async Task Success()
         {
-            var request = RequestRegisterUserJsonBuilder.Build();
+           var request = RequestRegisterUserJsonBuilder.Build();
 
+           var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            result.StatusCode.Should().Be(HttpStatusCode.Created);
 
         }
     }
